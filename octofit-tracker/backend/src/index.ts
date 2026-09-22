74fdb300c9dc286c533e84ac5f81b105177c4d00
Fedor Patrakov <1511800+freddygood@users.noreply.github.com>
2026-09-22 15:35:31 +0000

    fixed

--- octofit-tracker/backend/src/index.ts
+++ octofit-tracker/backend/src/index.ts
@@ -1,12 +1,1 @@
-import app from './app';
-import { connectDatabase } from './config/database';
-
-const port = 8000;
-
-app.listen(port, () => {
-  console.log(`OctoFit API listening on port ${port}`);
-});
-
-connectDatabase().catch((error: unknown) => {
-  console.error('Unable to connect to octofit_db:', error);
-});
+import './server';
